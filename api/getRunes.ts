07f2e786fa934207a509312f8ef39e87d0e821a5
polarDefender <patch.demon115@gmail.com>
2024-04-30 03:36:58 +0900

    update env key and added dotenv

--- api/getRunes.ts
+++ api/getRunes.ts
@@ -1,14 +1,12 @@
-require('dotenv').config()
+const NEXT_PUBLIC_UNISAT_API_KEY = process.env.NEXT_PUBLIC_UNISAT_API_KEY;
+const NEXT_PUBLIC_UNISAT_GET_INFOLIST_URL = process.env.NEXT_PUBLIC_UNISAT_GET_INFOLIST_URL;
 
-const UNISAT_API_KEY = process.env.UNISAT_API_KEY;
-const UNISAT_GET_INFOLIST_URL = process.env.UNISAT_GET_INFOLIST_URL;
-console.log(process.env.UNISAT_API_KEY)
 export const getRunes = async () => {
   try {
-    const response = await fetch(`${UNISAT_GET_INFOLIST_URL}`, {
+    const response = await fetch(`${NEXT_PUBLIC_UNISAT_GET_INFOLIST_URL}`, {
           method: 'GET', // This is the default.
           headers: {
-            'Authorization': `${UNISAT_API_KEY}`,
+            'Authorization': `Bearer ${NEXT_PUBLIC_UNISAT_API_KEY}`,
           },
         });
         return await response.json();
