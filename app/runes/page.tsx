168cf0f5145a321e8a33a47846f772fd32e747a7
polarDefender <patch.demon115@gmail.com>
2024-04-30 23:16:05 +0900

    updated getting data points function

--- app/runes/page.tsx
+++ app/runes/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { title } from "@/components/primitives";
-import { getRunes } from "@/api/getRunes";
+import { getInfoList } from "@/api/unisat";
+import { getTickers } from "@/api/bestinslot";
 import React, { useEffect, useState } from "react";
 import Table from "@/components/table";
 
@@ -9,9 +10,10 @@ export default function Runes() {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const test = await getRunes();
-      setData(test.data.detail);
-      console.log(test.data, "test");
+      const infoLists = await getInfoList();
+      const tickers = await getTickers();
+      setData(tickers);
+      console.log(tickers, "test");
     };
     fetchData();
   }, []);
